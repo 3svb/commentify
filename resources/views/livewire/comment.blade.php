83704bae4b5a9f3a73657afe11a9d4a833cd9e1c
3svb <driesvb@gmail.com>
2024-03-13 22:13:23 +0100

    test

--- resources/views/livewire/comment.blade.php
+++ resources/views/livewire/comment.blade.php
@@ -20,8 +20,8 @@
                     <p class="inline-flex items-center mr-3 text-sm text-gray-900 dark:text-white">
                         {{-- <img class="mr-2 w-6 h-6 rounded-full" src="{{ $comment->user->avatar() }}"
                             alt="{{ $comment->user->username }}"> --}}
-                        {{-- <x-avatar class="mr-2" :name="$comment->user->username" /> --}}
-                        {{-- {{ $comment->user->username }} --}}
+                        <x-avatar class="mr-2" :name="$comment->user->username" />
+                        {{ $comment->user->username }}
                     </p>
                     <p class="text-sm text-gray-600 dark:text-gray-400">
                         <time pubdate datetime="{{ $comment->presenter()->relativeCreatedAt() }}"
--- resources/views/livewire/comments.blade.php
+++ resources/views/livewire/comments.blade.php
@@ -1,34 +1,33 @@
 <div>
     {{-- <section class=""> --}}
-        <div class="mx-auto">
-            {{-- <div class="flex justify-between items-center mb-6">
+    <div class="mx-auto">
+        {{-- <div class="flex justify-between items-center mb-6">
                 <h2 class="text-lg lg:text-2xl font-bold text-gray-900 dark:text-white">Discussion
                     ({{$comments->count()}})</h2>
             </div> --}}
-            @auth
-            @include('commentify::livewire.partials.comment-form',[
-            'method'=>'postComment',
-            'state'=>'newCommentState',
-            'inputId'=> 'comment',
-            'inputLabel'=> 'Your comment',
-            'button'=>'Post',
-            'quote'=> $quote
+        @auth
+            @include('commentify::livewire.partials.comment-form', [
+                'method' => 'postComment',
+                'state' => 'newCommentState',
+                'inputId' => 'comment',
+                'inputLabel' => 'Your comment',
+                'button' => 'Post',
+                'quote' => $quote,
             ])
-            @else
+        @else
             {{-- <a class="mt-2 text-sm text-white" href="/login">Log in to comment!</a> --}}
             {{-- <p class="text-gray-400">Nog geen data</p> --}}
-            @endauth
-            @if($comments->count())
-            @foreach($comments as $comment)
-            <livewire:comment :$comment :key="$comment->id" />
+        @endauth
+        @if ($comments->count())
+            @foreach ($comments as $comment)
+                <livewire:comment :$comment :key="$comment->id" />
             @endforeach
-            {{$comments->links(data: ['scrollTo' => false])}}
-
-            @else
+            {{ $comments->links(data: ['scrollTo' => false]) }}
+        @else
             {{-- <p class="text-white">Nog geen gezever ...!</p> --}}
-            @endif
-        </div>
+        @endif
+    </div>
 
-        {{--
+    {{--
     </section> --}}
 </div>
